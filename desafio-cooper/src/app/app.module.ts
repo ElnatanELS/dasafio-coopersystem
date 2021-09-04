01771b1821db20313dc3bea9d707f1da9d8932e6
Elnatan Emanuel Lima Santos <elnatan.santos@neomed.tech>
2021-09-04 15:38:01 -0300

    criação do projeto

---
+++ desafio-cooper/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/shared/material-module/material-module.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
